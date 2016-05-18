540a4407b03cb6f0629da164c09418c3a145aa5c
LEEJeyeon <ljy@LEEJeyeonui-MacBook-Pro.local>
2016-05-18 15:07:38 +0900

    Add timer, stop button, reset button.

--- app/src/main/java/com/example/ljy/c04_view/MainActivity.java
+++ app/src/main/java/com/example/ljy/c04_view/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ljy.c04_view;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,12 +17,18 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-  LinearLayout mainLayout;
+  private LinearLayout mainLayout;
+  private TableLayout tableLayout;
+  private Button startButton;
+  private TextView timerView;
 
   private int selectedCount = 1;
   private Random random = new Random();
+  private long startTime;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +38,55 @@ public class MainActivity extends AppCompatActivity {
     mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
 
     TextView tv = new TextView(this);
-    tv.setText("Hello Active View!");
+    tv.setTextSize(20);
+    tv.setText("1 TO 25!");
 
     mainLayout.addView(tv);
 
-    TableLayout tableLayout = new TableLayout(this);
+    tableLayout = new TableLayout(this);
+    tableLayout.setStretchAllColumns(true);
     tableLayout.setShrinkAllColumns(true);
 
     ArrayList<Integer> randomNumbs = getRandomNumber();
 
     createTableRow(tableLayout, randomNumbs);
 
+    tableLayout.setVisibility(View.INVISIBLE);
     mainLayout.addView(tableLayout);
+
+    startButton = new Button(this);
+    startButton.setText("START!!");
+    startButton.setOnClickListener(getStartButtonListener());
+    mainLayout.addView(startButton);
+
+    timerView = new TextView(this);
+    timerView.setTextSize(40);
+    timerView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+    timerView.setText("00:00.000");
+
+    mainLayout.addView(timerView);
+
+    Button stopButton = new Button(this);
+    stopButton.setText("Stop!");
+    stopButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        stopTimer();
+      }
+    });
+
+    mainLayout.addView(stopButton);
+
+    Button resetButton = new Button(this);
+    resetButton.setText("Reset!");
+    resetButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        recreate();
+      }
+    });
+
+    mainLayout.addView(resetButton);
 
   }
 
@@ -106,11 +152,48 @@ public class MainActivity extends AppCompatActivity {
           button.setBackgroundColor(Color.GRAY);
           if (selectedCount == 26) {
             Toast.makeText(getApplicationContext(), " C L E A R ! ! ! ", Toast.LENGTH_SHORT).show();
-            recreate();
+            stopTimer();
+//            recreate();
           }
         }
       }
     };
   }
 
+  public View.OnClickListener getStartButtonListener() {
+    return new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        tableLayout.setVisibility(View.VISIBLE);
+        startTimer();
+      }
+    };
+  }
+
+  private boolean STOP;
+  private Handler timeHandler = new Handler() {
+    @Override
+    public void handleMessage(Message msg) {
+      super.handleMessage(msg);
+      timerView.setText(getTime());
+      timeHandler.sendEmptyMessage(0);
+    }
+  };
+
+  private void stopTimer() {
+    STOP = true;
+    timeHandler.removeMessages(0);
+  }
+
+  private void startTimer() {
+    startTime = SystemClock.elapsedRealtime();
+    timeHandler.sendEmptyMessage(0);
+  }
+
+  public String getTime() {
+    long now = SystemClock.elapsedRealtime();
+    long ell = now - startTime;
+
+    return String.format("%02d:%02d:%02d", ell/1000/60, (ell/1000)%60, (ell%1000)/10);
+  }
 }
